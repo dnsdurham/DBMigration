6754954a4ff2078a9324b5c902d7b144c1910e14
Doug Durham <github@dnsdurham.com>
2014-03-17 20:56:30 -0500

    Reading params from param file

--- DBMigration.Console/Program.cs
+++ DBMigration.Console/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Configuration;
 using System.IO;
+using System.Diagnostics;
 
 namespace DBMigration.Console
 {
@@ -16,10 +17,24 @@ namespace DBMigration.Console
             // Read in the params.txt file
             string sourcePath = ConfigurationManager.AppSettings["SourcePath"];
             string line;
+            string[] split;
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
             StreamReader file = new StreamReader(sourcePath + "params.txt");
             while ((line = file.ReadLine()) != null)
             {
-                // process the params file
+                // process the params file and store in a dictionary
+                //TODO: should we make sure that all of the expected parameters are in the params file?
+                split = line.Split(new Char[] {'='});
+                if (split.Length == 2)
+                {
+                    parameters.Add(split[0].Trim(), split[1].Trim());
+                }
+                else
+                {
+                    //write the line to debug
+                    //TODO: write this to a log file
+                    Trace.WriteLine("Invalid param line value: " + line);
+                }
             }
 
             // Insert the sql commands into the database
