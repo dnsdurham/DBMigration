2f1b280737c90eace9f4176769c43501c28bb86d
Doug Durham <dnsdurham@gmail.com>
2014-03-24 06:41:07 -0500

    Added project files for the sample command line app

---
+++ SampleCmdLineApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleCmdLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine("Argument Value: " + arg);
+            }
+
+            Console.WriteLine("Program complete");
+        }
+    }
+}
